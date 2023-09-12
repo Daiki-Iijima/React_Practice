89470ba4ea6cb0095d9809a5fcd933483de26b80
Daiki Iijima <daiki.applecreate@gmail.com>
2023-09-12 13:30:45 +0000

    [add] Reduxの状態管理機能を使った掲示板のサンプルを追加

---
+++ redux-board-app/src/App.js
@@ -0,0 +1,67 @@
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { addPost } from "./features/Posts";
+import { deletePost } from "./features/Posts";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
+  const postList = useSelector((state) => state.posts.value);
+  console.log(postList);
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(
+      addPost({
+        id: postList.length,
+        name: name,
+        content: content,
+      })
+    );
+
+    setName("");
+    setContent("");
+  };
+
+  const handleDeleteClick = (id) => {
+    dispatch(deletePost(id));
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h1>React-Redux掲示板</h1>
+      </div>
+      <div className="addPost">
+        <input
+          type="text"
+          placeholder="名前"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="text"
+          placeholder="投稿内容"
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+        />
+        <button onClick={() => handleClick()}>投稿</button>
+        <hr />
+      </div>
+      <div className="displayPosts">
+        {postList.map((post) => (
+          <div key={post.id} className="post">
+            <h1 className="postName">{post.name}</h1>
+            <h1 className="postContent">{post.content}</h1>
+            <button onClick={() => handleDeleteClick(post.id)}>削除</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
